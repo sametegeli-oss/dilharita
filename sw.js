23dcad8f137f7b87006643fedcd5494c4f0abcd8
sametegeli-oss <sametegeli@gmail.com>
2026-08-11 10:51:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
       indirilip bir sonraki açılışta devreye girer.
    4) Bildirim tıklama / push davranışı v3 ile aynı.
 */
-var SW_VERSION = "dh-sw-v61";   /* v61: Atlas hareketli bilesenleri */
+var SW_VERSION = "dh-sw-v62";   /* v62: tum ekranlarda sonuc animasyonlari */
 var CACHE = SW_VERSION;
 
 /* İlk açılışta hazır olması gereken minimum kabuk. Listeyi kısa tutun:
