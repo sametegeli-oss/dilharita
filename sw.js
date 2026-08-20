39aec7782d1cc828db28309d3e5a9c925ca914fb
sametegeli-oss <sametegeli@gmail.com>
2026-08-20 10:08:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
       indirilip bir sonraki açılışta devreye girer.
    4) Bildirim tıklama / push davranışı v3 ile aynı.
 */
-var SW_VERSION = "dh-sw-v131";  /* v131: word-popup AI anlam ve aciklama merkezi saglayici duzeltmesi */
+var SW_VERSION = "dh-sw-v132";  /* v132: kelime listesi anlamli gorsel hafiza semasi */
 var CACHE = SW_VERSION;
 
 /* İlk açılışta hazır olması gereken minimum kabuk. Listeyi kısa tutun:
