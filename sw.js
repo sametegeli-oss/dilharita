8a7d10490114040c33d5a9139695ddaaa25fe5df
sametegeli-oss <sametegeli@gmail.com>
2026-08-25 15:38:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
       indirilip bir sonraki açılışta devreye girer.
    4) Bildirim tıklama / push davranışı v3 ile aynı.
 */
-var SW_VERSION = "dh-sw-v173";  /* v173: Türkçe ters Shadowing, İngilizce rehber ve zaman çapası */
+var SW_VERSION = "dh-sw-v174";  /* v174: Altyazı zaman eşleme, karaoke aralığı ve TTS tekrar kilidi */
 var CACHE = SW_VERSION;
 
 /* İlk açılışta hazır olması gereken minimum kabuk. Listeyi kısa tutun:
