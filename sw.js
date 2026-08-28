6a60e68986e5580f3c59253c3a36cdabe126b975
sametegeli-oss <sametegeli@gmail.com>
2026-08-28 11:48:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
       indirilip bir sonraki açılışta devreye girer.
    4) Bildirim tıklama / push davranışı v3 ile aynı.
 */
-var SW_VERSION = "dh-sw-v199";  /* v199: YouTube P1/P2/P3 ayrımı ve eski modül sıra onarımı */
+var SW_VERSION = "dh-sw-v200";  /* v200: Video ve alt kontrol çubuğunu tek yapışkan blok yapma */
 var CACHE = SW_VERSION;
 
 /* İlk açılışta hazır olması gereken minimum kabuk. Listeyi kısa tutun:
