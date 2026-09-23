55de1f1a1c2e4a7e84427c6be4911b8d174aded6
sametegeli-oss <sametegeli@gmail.com>
2026-09-23 13:06:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
       indirilip bir sonraki açılışta devreye girer.
    4) Bildirim tıklama / push davranışı v3 ile aynı.
 */
-var SW_VERSION = "dh-sw-v284";  /* Cümle düğmeleri açılır panel; sekmeler+arama ⋯ menüsünde; ⇄ ve ⛶ kaldırıldı */
+var SW_VERSION = "dh-sw-v285";  /* Video başlık bloğu ⓘ düğmesiyle açılır, varsayılan kapalı */
 var CACHE = SW_VERSION;
 
 /* İlk açılışta hazır olması gereken minimum kabuk. Listeyi kısa tutun:
